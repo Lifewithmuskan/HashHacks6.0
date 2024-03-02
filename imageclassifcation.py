b20de3fa6ded7e9f1d0761d38e9f71de6f180747
Muskan singh <96696286+Lifewithmuskan@users.noreply.github.com>
2024-03-03 04:44:55 +0530

    Update imageclassifcation.py

--- imageclassifcation.py
+++ imageclassifcation.py
@@ -1,3 +1,17 @@
 import os
 import cv2
 import numpy as np
+def load_images(folder):
+  images = []
+  labels = []
+  for subfolder in os.listdir(folder):
+    subfolder_path = os.path.join(folder, subfolder)
+    for filename in os.listdir(subfolder_path):
+      img_path = os.path.join(subfolder_path, filename)
+      img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+      img = cv2.resize(img, (128, 128)) 
+      images.append(img)
+      labels.append(subfolder)
+      
+    return images, labels
+
